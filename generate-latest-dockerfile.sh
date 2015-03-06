819218862ae0765d41a31b873e928a0b2cd10fba
James <james@resin.io>
2015-03-06 16:58:35 +0000

    Add support for armv7hf images

---
+++ generate-latest-dockerfile.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+repos='armv7hf rpi'
+nodeVersions='0.9.12 0.10.36 0.11.16 0.12.0'
+
+for repo in $repos; do
+	for nodeVersion in $nodeVersions; do
+		echo $nodeVersion
+		baseVersion=$(expr match "$nodeVersion" '\([0-9]*\.[0-9]*\)')
+		dockerfilePath=$repo/$baseVersion
+		mkdir -p $dockerfilePath
+		sed -e s~#{FROM}~resin/$repo-buildpack-deps:jessie~g \
+			-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.tpl > $dockerfilePath/Dockerfile
+
+		mkdir -p $dockerfilePath/onbuild
+		sed -e s~#{FROM}~resin/$repo-node:$nodeVersion~g Dockerfile.onbuild.tpl > $dockerfilePath/onbuild/Dockerfile
+
+		
+
+		mkdir -p $dockerfilePath/wheezy
+		sed -e s~#{FROM}~resin/$repo-buildpack-deps:wheezy~g \
+			-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.tpl > $dockerfilePath/wheezy/Dockerfile
+
+		# Only for rpi-raspbian
+		if [ $repo == "rpi" ]; then
+			mkdir -p $dockerfilePath/slim
+			sed -e s~#{FROM}~resin/rpi-raspbian:jessie~g \
+				-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.slim.tpl > $dockerfilePath/slim/Dockerfile
+		fi 
+
+		# Only for armv7hf-debian
+		if [ $repo == "armv7hf" ]; then
+			mkdir -p $dockerfilePath/sid
+			sed -e s~#{FROM}~resin/$repo-buildpack-deps:sid~g \
+				-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.tpl > $dockerfilePath/sid/Dockerfile
+			mkdir -p $dockerfilePath/slim
+			sed -e s~#{FROM}~resin/armv7hf-debian:jessie~g \
+				-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.slim.tpl > $dockerfilePath/slim/Dockerfile
+		fi
+	done
+done
--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,38 +3,48 @@ set -e
 
 declare -A aliases
 aliases=(
-	[0.12]='0 latest'
+	[0.12.0]='0.12 latest' [0.10.36]='0.10' [0.11.16]='0.11' [0.9.12]='0.9' [0.8.28]='0.8'
 )
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-versions=( */ )
-versions=( "${versions[@]%/}" )
-url='git://github.com/resin-io-library/docker-node'
+repos=( "$@" )
+if [ ${#repos[@]} -eq 0 ]; then
+	repos=( */ )
+fi
+repos=( "${repos[@]%/}" )
 
 echo '# maintainer: Joyent Image Team <image-team@joyent.com> (@joyent)'
 echo '# maintainer: Trong Nghia Nguyen - resin.io <james@resin.io>'
 
-for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' -- "$version")"
-	fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
-	versionAliases=( $fullVersion $version ${aliases[$version]} )
-	
-	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
-	done
-	
-	for variant in onbuild slim wheezy; do
-		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+for repo in "${repos[@]}"; do
+
+	cd $repo
+	versions=( */ )
+	versions=( "${versions[@]%/}" )
+	cd ..
+	url='git://github.com/resin-io-library/docker-node'
+	for version in "${versions[@]}"; do
+		commit="$(git log -1 --format='format:%H' -- "$repo/$version")"
+		fullVersion="$(grep -m1 'ENV NODE_VERSION ' "$repo/$version/Dockerfile" | cut -d' ' -f3)"
+		versionAliases=( $fullVersion $version ${aliases[$version]} )
+
 		echo
 		for va in "${versionAliases[@]}"; do
-			if [ "$va" = 'latest' ]; then
-				va="$variant"
-			else
-				va="$va-$variant"
-			fi
-			echo "$va: ${url}@${commit} $version/$variant"
+			echo "$va: ${url}@${commit} $repo/$version"
+		done
+	
+		for variant in onbuild slim wheezy; do
+			commit="$(git log -1 --format='format:%H' -- "$repo/$version/$variant")"
+			echo
+			for va in "${versionAliases[@]}"; do
+				if [ "$va" = 'latest' ]; then
+					va="$variant"
+				else
+					va="$va-$variant"
+				fi
+				echo "$va: ${url}@${commit} $repo/$version/$variant"
+			done
 		done
 	done
 done
