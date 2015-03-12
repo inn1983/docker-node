c87f8ef10a7433f55ec6e7449492811b3dd2c318
James <james@resin.io>
2015-03-12 08:36:25 +0000

    Changing to armv7hf node binaries for armv7hf images

--- generate-latest-dockerfile.sh
+++ generate-latest-dockerfile.sh
@@ -5,13 +5,19 @@ repos='armv7hf rpi'
 nodeVersions='0.9.12 0.10.36 0.11.16 0.12.0'
 
 for repo in $repos; do
+	if [ $repo == "armv7hf" ]; then
+		target_arch='armv7hf'
+	else
+		target_arch='armv6hf'
+	fi
 	for nodeVersion in $nodeVersions; do
 		echo $nodeVersion
 		baseVersion=$(expr match "$nodeVersion" '\([0-9]*\.[0-9]*\)')
 		dockerfilePath=$repo/$baseVersion
 		mkdir -p $dockerfilePath
 		sed -e s~#{FROM}~resin/$repo-buildpack-deps:jessie~g \
-			-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.tpl > $dockerfilePath/Dockerfile
+			-e s~#{NODE_VERSION}~$nodeVersion~g \
+			-e s~#{TARGET_ARCH}~$target_arch~g Dockerfile.tpl > $dockerfilePath/Dockerfile
 
 		mkdir -p $dockerfilePath/onbuild
 		sed -e s~#{FROM}~resin/$repo-node:$nodeVersion~g Dockerfile.onbuild.tpl > $dockerfilePath/onbuild/Dockerfile
@@ -20,23 +26,27 @@ for repo in $repos; do
 
 		mkdir -p $dockerfilePath/wheezy
 		sed -e s~#{FROM}~resin/$repo-buildpack-deps:wheezy~g \
-			-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.tpl > $dockerfilePath/wheezy/Dockerfile
+			-e s~#{NODE_VERSION}~$nodeVersion~g \
+			-e s~#{TARGET_ARCH}~$target_arch~g Dockerfile.tpl > $dockerfilePath/wheezy/Dockerfile
 
 		# Only for rpi-raspbian
 		if [ $repo == "rpi" ]; then
 			mkdir -p $dockerfilePath/slim
 			sed -e s~#{FROM}~resin/rpi-raspbian:jessie~g \
-				-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.slim.tpl > $dockerfilePath/slim/Dockerfile
+				-e s~#{NODE_VERSION}~$nodeVersion~g \
+				-e s~#{TARGET_ARCH}~$target_arch~g Dockerfile.slim.tpl > $dockerfilePath/slim/Dockerfile
 		fi 
 
 		# Only for armv7hf-debian
 		if [ $repo == "armv7hf" ]; then
 			mkdir -p $dockerfilePath/sid
 			sed -e s~#{FROM}~resin/$repo-buildpack-deps:sid~g \
-				-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.tpl > $dockerfilePath/sid/Dockerfile
+				-e s~#{NODE_VERSION}~$nodeVersion~g \
+				-e s~#{TARGET_ARCH}~$target_arch~g Dockerfile.tpl > $dockerfilePath/sid/Dockerfile
 			mkdir -p $dockerfilePath/slim
 			sed -e s~#{FROM}~resin/armv7hf-debian:jessie~g \
-				-e s~#{NODE_VERSION}~$nodeVersion~g Dockerfile.slim.tpl > $dockerfilePath/slim/Dockerfile
+				-e s~#{NODE_VERSION}~$nodeVersion~g \
+				-e s~#{TARGET_ARCH}~$target_arch~g Dockerfile.slim.tpl > $dockerfilePath/slim/Dockerfile
 		fi
 	done
 done
